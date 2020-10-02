b6aa7a273165ce2e6719e49b02a34660a4d91047
Casper Lindberg Petersson <caspet@elevad.umea.se>
2020-10-02 13:29:37 +0200

    Updated __init__.py to be a package

--- PyCollision2D/__init__.py
+++ PyCollision2D/__init__.py
@@ -1,3 +1,3 @@
-from vector import Vector
-from rect import Rect
-from ray import Ray
+from PyCollision2D.vector import Vector
+from PyCollision2D.rect import Rect
+from PyCollision2D.ray import Ray
